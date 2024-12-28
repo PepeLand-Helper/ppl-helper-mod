55d951f2f3f835b547d679f28aaf937bf20ee8a8
Kel Cuprum <me@kelcuprum.ru>
2024-12-28 10:14:31 +0300

    Обновление файлов [1.0.0-beta.2] - Добавлена кнопка Лобби в меню паузы [Показывается только на PPL и не в самом лобби] - Добавлен парсер миров - Добавлены значения локализации   - pplhelper.world - Мир, в котором вы находитесь   - pplhelper.world_short - Тоже самое, но сокращенно

--- src/main/java/ru/kelcuprum/pplhelper/PepelandHelper.java
+++ src/main/java/ru/kelcuprum/pplhelper/PepelandHelper.java
@@ -7,14 +7,17 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.repository.Pack;
 import org.apache.logging.log4j.Level;
 import org.lwjgl.glfw.GLFW;
+import org.meteordev.starscript.value.Value;
 import ru.kelcuprum.alinlib.AlinLib;
 import ru.kelcuprum.alinlib.AlinLogger;
 import ru.kelcuprum.alinlib.api.KeyMappingHelper;
+import ru.kelcuprum.alinlib.api.events.alinlib.LocalizationEvents;
 import ru.kelcuprum.alinlib.api.events.client.ClientLifecycleEvents;
 import ru.kelcuprum.alinlib.api.events.client.ClientTickEvents;
 import ru.kelcuprum.alinlib.api.events.client.TextureManagerEvent;
@@ -61,6 +64,58 @@ public class PepelandHelper implements ClientModInitializer {
 
                 new ButtonBuilder(Component.translatable("pplhelper.pack")).setOnPress((s) -> AlinLib.MINECRAFT.setScreen(new UpdaterScreen().build(parent))).setSprite(Icons.PACK_INFO).setSize(20, 20)
         };
+    }
+
+    public static Worlds getWorld(){
+        if((AlinLib.MINECRAFT.getCurrentServer() == null || !AlinLib.MINECRAFT.getCurrentServer().ip.contains("pepeland.net")) || AlinLib.MINECRAFT.gui.getTabList().header == null) return null;
+        String[] args = AlinLib.MINECRAFT.gui.getTabList().header.getString().split("\n");
+        StringBuilder world = new StringBuilder();
+        for(String arg : args){
+            if(arg.contains("Мир:")){
+                String[] parsed = arg.replace("Мир:", "").replaceAll("[^A-Za-zА-Яа-я #0-9]", "").split(" ");
+                boolean first = true;
+                for(String parsed_arg : parsed) {
+                    if(!parsed_arg.isBlank()) {
+                        world.append(first ? "" : " ").append(parsed_arg);
+                        if(first) first = false;
+                    }
+                }
+            }
+        }
+        return switch (world.toString()){
+            case "Лобби" -> Worlds.LOBBY;
+            case "Постройки #1" -> Worlds.CONSTRUCTIONS_1;
+            case "Постройки #2" -> Worlds.CONSTRUCTIONS_2;
+            case "Ресурсы" -> Worlds.RESOURCE;
+            case "Фермы" -> Worlds.FARM;
+            case "Торговля" -> Worlds.TRADE;
+            default -> null;
+        };
+    }
+
+    public enum Worlds {
+        LOBBY(Component.translatable("pplhelper.world.lobby"), "Лобби"),
+        RESOURCE(Component.translatable("pplhelper.world.resource"), "МР"),
+        CONSTRUCTIONS_1(Component.translatable("pplhelper.world.constructions.1"), "МП1"),
+        CONSTRUCTIONS_2(Component.translatable("pplhelper.world.constructions.2"), "МП2"),
+        FARM(Component.translatable("pplhelper.world.farm"), "МФ"),
+        TRADE(Component.translatable("pplhelper.world.trade"), "ТЦ"),
+        END(Component.translatable("pplhelper.world.end"), "Энд");
+        Component title;
+        String shortName;
+        Worlds(Component title, String shortName){
+            this.title = title;
+            this.shortName = shortName;
+        }
+    }
+
+    public static void executeCommand(LocalPlayer player, String command) {
+        if (command.startsWith("/")) {
+            command = command.substring(1);
+            player.connection.sendCommand(command);
+        } else {
+            player.connection.sendChat(command);
+        }
     }
 
     @Override
@@ -128,6 +183,13 @@ public class PepelandHelper implements ClientModInitializer {
         });
         ClientLifecycleEvents.CLIENT_STOPPING.register((s) -> TextureHelper.saveMap());
         TextureManagerEvent.INIT.register(TextureHelper::loadTextures);
+        LocalizationEvents.DEFAULT_PARSER_INIT.register(starScript -> starScript.ss.set("pplhelper.world", () -> {
+            Worlds world = getWorld();
+            return Value.string(world == null ? "" : world.title.getString());
+        }).set("pplhelper.world_short", () -> {
+            Worlds world = getWorld();
+            return Value.string(world == null ? "" : world.shortName);
+        }));
     }
 
     public static boolean onlyEmotesCheck(){
---
+++ src/main/java/ru/kelcuprum/pplhelper/mixin/PauseScreenMixin.java
@@ -0,0 +1,65 @@
+package ru.kelcuprum.pplhelper.mixin;
+
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import ru.kelcuprum.abi.ActionBarInfo;
+import ru.kelcuprum.alinlib.AlinLib;
+import ru.kelcuprum.alinlib.config.Localization;
+import ru.kelcuprum.alinlib.gui.Icons;
+import ru.kelcuprum.alinlib.gui.components.builder.button.ButtonBuilder;
+import ru.kelcuprum.alinlib.gui.toast.ToastBuilder;
+import ru.kelcuprum.alinlib.info.World;
+import ru.kelcuprum.pplhelper.PepelandHelper;
+
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+import static ru.kelcuprum.alinlib.gui.Icons.DONT;
+import static ru.kelcuprum.pplhelper.PepelandHelper.Icons.WHITE_PEPE;
+
+@Mixin(PauseScreen.class)
+public class PauseScreenMixin extends Screen {
+
+    protected PauseScreenMixin(Component component) {
+        super(component);
+    }
+
+    @Inject(method = "createPauseMenu", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/GridLayout;visitWidgets(Ljava/util/function/Consumer;)V"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    void createPauseMenu(CallbackInfo ci, GridLayout gridLayout){
+        if(AlinLib.MINECRAFT.hasSingleplayerServer() || AlinLib.MINECRAFT.isLocalServer() || AlinLib.MINECRAFT.getCurrentServer() == null) return;
+        if(AlinLib.MINECRAFT.getCurrentServer().ip.contains("pepeland.net") && PepelandHelper.getWorld() != PepelandHelper.Worlds.LOBBY) {
+            PepelandHelper.getWorld();
+            if (gridLayout != null) {
+                final List<AbstractWidget> buttons = ((AccessorGridLayout) gridLayout).getChildren();
+                int vanillaButtonsY = this.height / 4 + 72 - 16 + 1;
+                for(AbstractWidget widget : buttons){
+                    if(widget.getMessage().contains(Component.translatable("menu.feedback")) || widget.getMessage().contains(Component.translatable("menu.sendFeedback"))){
+                        vanillaButtonsY = widget.getY();
+                    }
+                }
+                buttons.add(new ButtonBuilder(Component.translatable("pplhelper.lobby"))
+                        .setOnPress((s) -> {
+                            PepelandHelper.executeCommand(AlinLib.MINECRAFT.player, "/lobby");
+                            new ToastBuilder().setTitle(Component.translatable("pplhelper")).setMessage(Component.translatable("pplhelper.lobby.toast")).setIcon(WHITE_PEPE).buildAndShow();
+                        })
+                        .setSprite(Icons.CLOWNFISH)
+                        .setPosition(this.width / 2 - 4 - 100 - 2 - 20, vanillaButtonsY)
+                        .setSize(20, 20)
+                        .build());
+            }
+        }
+    }
+}
